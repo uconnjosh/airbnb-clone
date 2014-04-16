2a0989b78b51252d025804f98b0b9a2dbe09a3a8
Angela Yen & Josh Paul <student@epicodus.com>
2014-04-16 12:00:49 -0700

    add ability to fully edit user profile w devise and strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :bio, :avatar, :owner]
+    devise_parameter_sanitizer.for(:account_update) << [:name, :bio, :avatar, :owner]
   end
+
 end
