68121f8b0d28b8f996909562810079f1d250363f
Angela Yen & Josh Paul <student@epicodus.com>
2014-04-16 11:31:19 -0700

    utilizes devise for user authentication with additional parameters of name, bio, avatar, and owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ end
 
 private
   def user_params
-    params.require(:user).permit(:name, :bio, :avatar, :owner)
+    params.require(:user).permit(:name, :bio, :avatar, :owner, :email, :password, :password_confirmation)
   end
 
 end
