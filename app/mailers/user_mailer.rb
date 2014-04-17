b37ddd1c1acc96973f354cedaf83ddd7c4737224
Angela Yen & Josh Paul <ayen10@gmail.com>
2014-04-17 09:32:17 -0700

    send new users a signup confirmation email, functionality not tested yet.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,19 @@
+class UserMailer < ActionMailer::Base
+  default from: "joshandangela@JanBnB.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup_confirmation.subject
+  #
+  def signup_confirmation(user)
+    @user = user
+    if @user.owner == true
+      @greeting = "We are happy to be welcoming another property owner to our team"
+    else
+      @greeting = "Thanks for joining! We have lots of cool properties for you to check out!"
+    end
+
+    mail to: user.email, subject: "Thanks for joining JanBnB!"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,23 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  after_create :send_welcome_message
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :properties
-
   validates :name, :presence => true
   validates :bio, :presence => true
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
+  def send_welcome_message
+    UserMailer.signup_confirmation(self)
+  end
 
    def greeting
     if Time.now.hour >= 17
